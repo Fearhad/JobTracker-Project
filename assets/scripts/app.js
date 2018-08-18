bc3333a68ec1b908ec8b8be6db0437b0a1e21208
Fearhad <allan.hartigan@gmail.com>
2018-08-18 15:59:06 -0400

    added google map functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,7 @@
+var jobLocation = "Wirecutter";
+var map = "<img width='600' src='https://maps.googleapis.com/maps/api/staticmap?center=" + jobLocation + "&zoom=8&scale=13&size=600x300&maptype=roadmap&format=png&visual_refresh=true'>"
+$(document).ready(function() {
+$("#googleMap").append(map);
+console.log(map)
+});
 
