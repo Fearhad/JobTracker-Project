135df8a1dfb5adfe478185e0db2afb1859779af6
Ilham Hafizovic <ilham.hafizovic@gmail.com>
2018-08-23 18:21:51 -0400

    added ajaxprefilter to script.js file that Andy gave us on Tuesday. This will allow us to run the Ajax call to the goremote.io api from Github without the CORS Chrome plugin.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,6 +16,12 @@ var globalJobObject;
 var globalFavJobObject = [];
 var favoriteJobs = [];
 
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+}); 
+
 $.ajax({
     url: queryURL,
     method: "GET"
